28c35f1fdfdc69cff8d6281955760710cdfb68ac
parihargaurav <parihargaurav527@gmail.com>
2024-03-04 01:04:57 +0530

    first commit

--- 01.js
+++ 01.js
@@ -1,5 +1,7 @@
 // Hoisting concept using global execution context
-console.log("Value of Age is", age);
+
+console.log("Value of Age is", age); // output: undefined
 var age = 20;
 
-console.log("Value of Age is", age);
+console.log("Value of Age is", age); // output: 20
+
---
+++ 02.js
@@ -0,0 +1,9 @@
+age  = 100;
+console.log("Value of age is ", age); // output: 100
+var age = 30;
+console.log("Value of age is ", age); // output : 30
+
+
+//  if we make variable using let ,  const then situation occur is - temporal dead zone
+// but in var we don't have  temporal dead zone
+//  var follows hoisting
